d7f63ebe35ba39be67a2ad8f4a891570812a007d
zhi_jun <liangzhijun1216@gmail.com>
2024-11-02 20:26:12 -0400

    working on dashboard middle content

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -7,9 +7,17 @@ const Nav = () => {
   // State to manage the active link and content
   const [activeLink, setActiveLink] = useState("/");
 
+  // State to manage which dropdown is open
+  const [openDropdown, setOpenDropdown] = useState(null);
+
   // Function to handle link click
   const handleLinkClick = (path: string) => {
     setActiveLink(path);
+  };
+
+  // Function to toggle dropdown
+  const toggleDropdown = (index) => {
+    setOpenDropdown(openDropdown === index ? null : index); // Toggle the dropdown
   };
 
   return (
@@ -46,113 +54,98 @@ const Nav = () => {
       <div className="">
         {activeLink === "/" ? (
           // Home Page Content
-          <div className="bigPuzz">
-            <div className="puzzles">
-              <p className="PuzWeek">Puzzles of the week</p>
-              <div className="Prepuz">
-                <div className="puz">
-                  <img src="/puzzle.svg" alt="Puzzle" />
-                  <p>Sum it up</p>
-                  <p>Easy</p>
+          <div className="BigPuzz">
+            <div className="bigPuzz">
+              <div className="puzzles">
+                <p className="PuzWeek">Puzzles of the week</p>
+                <div className="Prepuz">
+                  <div className="puz">
+                    <img src="/puzzle.svg" alt="Puzzle" />
+                    <p>Sum it up</p>
+                    <p>Easy</p>
+                  </div>
+                  <div className="puz">
+                    <img src="/puzzle.svg" alt="Puzzle" />
+                    <p>Sum it up</p>
+                    <p>Easy</p>
+                  </div>
+                  <div className="puz">
+                    <img src="/puzzle.svg" alt="Puzzle" />
+                    <p>Sum it up</p>
+                    <p>Easy</p>
+                  </div>
+                  <div className="puz">
+                    <img src="/puzzle.svg" alt="Puzzle" />
+                    <p>Sum it up</p>
+                    <p>Easy</p>
+                  </div>
+                  <div className="puz">
+                    <img src="/puzzle.svg" alt="Puzzle" />
+                    <p>Sum it up</p>
+                    <p>Easy</p>
+                  </div>
+                  <div className="puz">
+                    <img src="/puzzle.svg" alt="Puzzle" />
+                    <p>Sum it up</p>
+                    <p>Easy</p>
+                  </div>
+                  <div className="puz">
+                    <img src="/puzzle.svg" alt="Puzzle" />
+                    <p>Sum it up</p>
+                    <p>Easy</p>
+                  </div>
                 </div>
-                <div className="puz">
-                  <img src="/puzzle.svg" alt="Puzzle" />
-                  <p>Sum it up</p>
-                  <p>Easy</p>
-                </div>
-                <div className="puz">
-                  <img src="/puzzle.svg" alt="Puzzle" />
-                  <p>Sum it up</p>
-                  <p>Easy</p>
-                </div>
-                <div className="puz">
-                  <img src="/puzzle.svg" alt="Puzzle" />
-                  <p>Sum it up</p>
-                  <p>Easy</p>
-                </div>
-                <div className="puz">
-                  <img src="/puzzle.svg" alt="Puzzle" />
-                  <p>Sum it up</p>
-                  <p>Easy</p>
-                </div>
-                <div className="puz">
-                  <img src="/puzzle.svg" alt="Puzzle" />
-                  <p>Sum it up</p>
-                  <p>Easy</p>
-                </div>
-                <div className="puz">
-                  <img src="/puzzle.svg" alt="Puzzle" />
-                  <p>Sum it up</p>
-                  <p>Easy</p>
+              </div>
+
+              {/* dropdown */}
+              <div className="puzzles">
+                <p className="PuzWeek">FAQ</p>
+                <div className="drop">
+                  {Array.from({ length: 7 }).map((_, index) => (
+                    <div className="dropdown" key={index}>
+                      <button
+                        className="dropbtn"
+                        onClick={() => toggleDropdown(index)}
+                      >
+                        What is this supposed to be?
+                        <span className="arrow">
+                          <img src="/arrow_down.svg" alt="Arrow" />
+                        </span>
+                      </button>
+                      {openDropdown === index && (
+                        <div className="dropdown-content">
+                          <p>This is a dropdown content!</p>
+                          <p>You can add more information here.</p>
+                        </div>
+                      )}
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
+            <div className="midPuzz">
+              <div className="puzzles2">
+                <p className="code">1v1 Speed Coding</p>
+                <p className="code2">
+                  Challenge your coding skills in real-time with head to head
+                  speed coding matches
+                </p>
+                <button className="play">Play</button>
+              </div>
+              <div className="puzzles4">
+                <p>Leaderboard</p>
 
-            <div className="puzzles">
-              <p className="PuzWeek">FAQ</p>
-              <div className="drop">
-                {" "}
-                <div className="dropdown">
-                  <button className="dropbtn">
-                    What is this supposed to be?
-                    <span className="arrow">
-                      <img src="/arrow_down.svg"></img>
-                    </span>
-                  </button>
-                  <div className="dropdown-content">
-                    <p>This is a dropdown content!</p>
-                    <p>You can add more information here.</p>
-                  </div>
-                </div>
-                <div className="dropdown">
-                  <button className="dropbtn">
-                    What is this supposed to be?
-                    <span className="arrow">
-                      <img src="/arrow_down.svg"></img>
-                    </span>
-                  </button>
-                  <div className="dropdown-content">
-                    <p>This is a dropdown content!</p>
-                    <p>You can add more information here.</p>
-                  </div>
-                </div>
-                <div className="dropdown">
-                  <button className="dropbtn">
-                    What is this supposed to be?
-                    <span className="arrow">
-                      <img src="/arrow_down.svg"></img>
-                    </span>
-                  </button>
-                  <div className="dropdown-content">
-                    <p>This is a dropdown content!</p>
-                    <p>You can add more information here.</p>
-                  </div>
-                </div>
-                <div className="dropdown">
-                  <button className="dropbtn">
-                    What is this supposed to be?
-                    <span className="arrow">
-                      <img src="/arrow_down.svg"></img>
-                    </span>
-                  </button>
-                  <div className="dropdown-content">
-                    <p>This is a dropdown content!</p>
-                    <p>You can add more information here.</p>
-                  </div>
-                </div>
-                <div className="dropdown">
-                  <button className="dropbtn">
-                    What is this supposed to be?
-                    <span className="arrow">
-                      <img src="/arrow_down.svg"></img>
-                    </span>
-                  </button>
-                  <div className="dropdown-content">
-                    <p>This is a dropdown content!</p>
-                    <p>You can add more information here.</p>
-                  </div>
+                <div className="leaderboard"> 
+                  <p>Rank</p>
+                  <p>Name</p>
+                  <p>Speed</p>
+                  <p>Difficulity</p>
                 </div>
               </div>
+            </div>
+            <div className="midPuzz">
+              <div className="puzzles3"></div>
+              <div className="puzzles3"></div>
             </div>
           </div>
         ) : (
